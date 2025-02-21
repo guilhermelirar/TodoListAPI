894e23bd28d7cfe5b5aca3a0c7d9b37a9c0e1ef7
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-02-21 20:36:00 -0300

    SQLAlchemy setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(config_class = 'app.config.Config') -> Flask:
     app = Flask(__name__)
-    
+
     app.config.from_object(config_class)
     
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
     return app
--- app/config.py
+++ app/config.py
@@ -2,3 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'very_secure_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or f'sqlite:///todo.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
