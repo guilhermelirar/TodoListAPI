a76d9d3c7469b857866a226dba950a5a98bc8073
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-02-21 20:48:10 -0300

    Starting pytest setup

--- app/config.py
+++ app/config.py
@@ -2,6 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'very_secure_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or f'sqlite:///todo.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///todo.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class TestConfig:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:memory'
+    TESTING = True
---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from app import create_app, db
+
+@pytest.fixture
+def app():
+    app = create_app(config_class='app.config.TestConfig')
+    with app.app_context():
+        db.create_all()
+
+    yield app
+
+    with app.app_context():
+        db.session.remove()
+        db.drop_all()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
