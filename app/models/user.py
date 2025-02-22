4e44e8459c20d767f8e8f9f3e24a1c4a7380b8ba
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-02-22 20:12:08 -0300

    feat: Email validation and password hashing on User creation

--- app/models/user.py
+++ app/models/user.py
@@ -2,8 +2,22 @@
 import re
 from sqlalchemy.orm import Mapped, mapped_column
 from app import db
+from werkzeug.security import generate_password_hash
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+    @staticmethod
+    def is_email_valid(email: str) -> bool:
+        regex = r"^[\w\.-]+@([\w\-]+\.)+[\w\-]{2,4}$"
+        return bool(re.match(regex, email))
+    
+    def __init__(self, name, email, password):
+        if not User.is_email_valid(email):
+            raise ValueError("Invalid Email")
+        
+        self.name = name 
+        self.email = email
+        self.password = generate_password_hash(password)
--- tests/user_test.py
+++ tests/user_test.py
@@ -6,5 +6,15 @@ def test_email_validation():
     email = "invalid email"
     password = "password123"
 
-    with raises(ValueError, match='Invald Email'):
+    with raises(ValueError, match="Invalid Email"):
         User(name, email, password)
+
+def test_password_is_hashed():
+    name = "Name"
+    email = "valid@email.com"
+    password = "password123"
+
+    user = User(name, email, password)
+    assert user.name == name
+    assert user.email == email
+    assert user.password != password
