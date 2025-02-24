286ca13eb05daaee2d13f04c28f2bec4226b1561
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-02-24 11:12:09 -0300

    fixture for existing user in database

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "/.."))
 
 import pytest
 from app import create_app, db
+from app.models import User
 
 @pytest.fixture
 def app():
@@ -21,3 +22,17 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def existing_user(app): 
+    new_user: User
+
+    with app.app_context():
+        db.session.query(User).filter_by(email="existing@email.com").delete()
+        db.session.commit()
+
+        new_user = User("Existing", "existing@email.com", "password123")
+        db.session.add(new_user)
+        db.session.commit()
+
+    return new_user
