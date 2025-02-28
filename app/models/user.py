5b47f977f7ab22afac17e35ae62e4d49dbc424b3
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-02-28 18:07:37 -0300

    fix: password field in User; feat: password checking method

--- app/models/user.py
+++ app/models/user.py
@@ -2,18 +2,22 @@
 import re
 from sqlalchemy.orm import Mapped, mapped_column
 from app import db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
 
     @staticmethod
     def is_email_valid(email: str) -> bool:
         regex = r"^[\w\.-]+@([\w\-]+\.)+[\w\-]{2,4}$"
         return bool(re.match(regex, email))
     
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password, password)
+
     def __init__(self, name, email, password):
         if not User.is_email_valid(email):
             raise ValueError("Invalid Email")
