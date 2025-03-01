beefa266ed4918625bf04e93f7b35ba31f9f64e2
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-03-01 19:02:49 -0300

    handling invalid access tokens on /todos

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,5 +1,6 @@
 #app/routes/auth_routes.py
 from flask import Blueprint, request, Response, jsonify
+from app.services.auth_service import user_from_access_token, UnauthorizedTokenError
 
 todo_bp = Blueprint('todos', __name__)
 
@@ -21,5 +22,12 @@ def todos() -> tuple[Response, int]:
         return unauthorized
 
     token: str = auth_header.split()[1]
+
+    try:
+        user: dict = user_from_access_token(token)
+    except UnauthorizedTokenError as e:
+        return jsonify({
+            "message": str(e)
+        }), 401
 
     return jsonify(), 0
