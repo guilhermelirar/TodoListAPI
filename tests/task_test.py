501133aeb15d9b24b5cb1e44035a2484667a53ed
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-03-03 20:35:25 -0300

    fixed comments on test

--- tests/task_test.py
+++ tests/task_test.py
@@ -10,12 +10,14 @@ def test_create_task_requires_authorization(client: FlaskClient):
     assert response.status_code == 401
     assert response.get_json()["message"] == "Unauthorized"
 
+
 def test_create_task_rejects_invalid_token(client: FlaskClient):
     response: TestResponse = client.post("/todos", json=task, 
                                          headers={"Authorization":"Bearer invalid"})
 
     assert response.status_code == 401
     assert response.get_json()["message"] == "Unauthorized"
+
 
 def test_create_task_with_invalid_fields(client: FlaskClient, 
                                          existing_user_tokens: dict):
@@ -24,6 +26,7 @@ def test_create_task_with_invalid_fields(client: FlaskClient,
 
     assert response.status_code == 400
     assert response.get_json()["message"] == "Missing fields"
+
 
 def test_create_task_with_success(client: FlaskClient, existing_user_tokens: dict):
     headers = {"Authorization": f"Bearer {existing_user_tokens['access_token']}"}
@@ -35,12 +38,12 @@ def test_create_task_with_success(client: FlaskClient, existing_user_tokens: dic
     res_json = response.get_json()
     assert len(res_json) == 3 
 
+
 def test_update_nonexistent_task(client: FlaskClient, existing_user_tokens: dict):
     headers = {"Authorization": f"Bearer {existing_user_tokens['access_token']}"}
     response: TestResponse = client.put("/todos/0", json=task_2, headers=headers)
 
-    # To-do item created with success
+    # Tas is not found
     assert response.status_code == 404
-    # Details of created item are present
     assert response.get_json()["message"] == "Task not found"
  
