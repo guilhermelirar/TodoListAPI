803927211fa229829296f146b978bcf3df2f59f7
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-03-21 20:08:41 -0300

    removing hello route

--- run.py
+++ run.py
@@ -3,11 +3,4 @@ from app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-
-    @app.route('/hello', methods=['GET'])
-    def hello():
-        return jsonify({
-            "message" : "Hello World!"
-        }), 200
-
     app.run(host='0.0.0.0', debug=True)
