a3a65c7c45f623d6aa86de5668dfa883c5eb35d9
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-03-21 20:09:27 -0300

    init with limiter and handling error 429 (too many requests)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from .extensions import limiter
 
 db = SQLAlchemy()
 
@@ -9,6 +10,20 @@ def create_app(config_class = 'app.config.Config') -> Flask:
     app.config.from_object(config_class)
     
     db.init_app(app)
+    limiter.init_app(app)
+
+    @app.route('/', methods=['GET'])
+    @limiter.limit("1 per minute")
+    def hello():
+        return jsonify({
+            "message": "Hello World!"
+        }), 200
+
+    @app.errorhandler(429)
+    def too_many(e):
+        return jsonify({
+            "message": "Too many requests"
+        }), 429
 
     with app.app_context():
         db.create_all()
