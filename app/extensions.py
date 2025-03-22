bb16372294114feac3180d8df69f7d79cedda180
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-03-22 19:47:18 -0300

    limiter: new limiter key function (uses id when available)

--- app/extensions.py
+++ app/extensions.py
@@ -1,10 +1,12 @@
-
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask import g
 
+def rate_limit_key():
+    return str(g.get("user_id", get_remote_address()))
 
 limiter = Limiter(
-    get_remote_address,
+    rate_limit_key,
     default_limits=["200 per day", "50 per hour"],
     storage_uri="memory://"
 )
