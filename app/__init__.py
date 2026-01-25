6ee2cfdcd06a1b43ba26075bdc3e7f5f2d5277fb
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-01-25 18:17:35 -0300

    separating app factory into functions

--- app/__init__.py
+++ app/__init__.py
@@ -4,60 +4,35 @@ from .extensions import limiter
 from flasgger import Swagger
 from flask_migrate import Migrate
 from .errors import ServiceError
-from werkzeug.exceptions import HTTPException
+from .errorhandlers import set_error_handlers
 
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app(config_class = 'app.config.Config') -> Flask:
-    app = Flask(__name__)
-
-    app.config.from_object(config_class)
-    
+def register_extensions(app: Flask) -> None:
     db.init_app(app)
+    migrate.init_app(app, db)
     limiter.init_app(app)
 
-    @app.route('/', methods=['GET'])
-    @limiter.limit("1 per minute")
-    def hello():
-        return jsonify({
-            "message": "Hello World!"
-        }), 200
-
-    @app.errorhandler(ServiceError)
-    def service_error(e):
-        return jsonify({
-            "message": e.message
-        }), e.status_code
-
-
-    @app.errorhandler(429)
-    def too_many(e):
-        return jsonify({
-            "message": "Too many requests"
-        }), 429
-    
-    @app.errorhandler(500)
-    def internal_server_error(e):
-        return jsonify({"message": "Internal Server Error"}), 500
-    
-    @app.errorhandler(HTTPException)
-    def handle_http_exception(e):
-        return jsonify({"message": "Invalid JSON body"}), e.code
-        
-    migrate.init_app(app, db)
-
-    # Blueprints registration
-    from app.routes import auth_bp, todo_bp
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(todo_bp)
-
-    # API documentation with Swagger
     Swagger(app, template={
         "info": {
             "title": "Todo List API",
             "version": "1.0.0"
         }
     })
-        
+
+def register_blueprints(app):
+    from app.routes import auth_bp, todo_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(todo_bp)
+
+def create_app(config_class = 'app.config.Config') -> Flask:
+    app = Flask(__name__)
+
+    app.config.from_object(config_class)
+
+    register_extensions(app)
+    register_blueprints(app)
+    set_error_handlers(app)
+
     return app
